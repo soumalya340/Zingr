6f20bca87ba5a8a13d33d7c4964af599fb77b9bd
Shubham Prajapati <shubhamprajapati032@gmail.com>
2024-06-27 14:09:28 +0530

    raid page ui change

--- app/voyager/raids/page.tsx
+++ app/voyager/raids/page.tsx
@@ -29,21 +29,17 @@ const Raids = () => {
         }
     ];
     return (
-        <div className='mx-20'>
+        <div className=' bg-gradient-custom'>
             <Navbar />
             
            
-            <div className="flex flex-col bg-[#FFFCF9]">
+            <div className="flex flex-col ">
                 <div className="flex flex-col md:flex-row md:space-x-10">
                     <div className="h-[640px] w-[60%] overflow-hidden">
-                        <img
-                            src="/raids.png"
-                            alt="Globe with suitcases and hot air balloons"
-                            className="object-cover h-full w-full"
-                        />
+                        
                     </div>
                     <div className=" text-center  md:text-left md:mt-0">
-                        <h1 className="text-5xl mt-60 text-gray-800">
+                        <h1 className="text-5xl mt-60 text-white">
                             Embark on Unforgettable Adventures
                         </h1>
                         <a href="/voyager/soloraids"><button className="mt-5 px-5 py-3 text-white bg-[#101521] hover:bg-[#2a73ae] rounded-lg shadow-md">
@@ -58,7 +54,7 @@ const Raids = () => {
                     </h1>
                 </div>
 
-                <div className="bg-[#D7E58D] min-h-screen p-10 mt-20">
+                <div className=" min-h-screen p-10 mt-20">
                     <div className="grid grid-cols-1 md:grid-cols-2">
                         <div className="flex flex-col items-center p-5">
                             <button className="text-3xl font-bold mb-2">Solo raids<span className="text-3xl">↗</span></button>
